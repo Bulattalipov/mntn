c387857a6d172351d5a864fe6ad047c9c1b10cba
Bulat <bulat0talipov@gmail.com>
2022-02-04 16:28:49 +0300

    added adaptive page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $ (function (){
-  
+
 });
+
+
+let btn = document.querySelector(".header__btn-box");
+let menu = document.querySelector('.menu');
+
+btn.addEventListener("click", function(){
+  btn.classList.toggle('header__btn-box--active');
+  menu.classList.toggle("menu--active");
+})
+
